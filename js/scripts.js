a217fa95307978b9dabe9b4ef37ce9313e86ce20
NeverSettels <settels.eric@gmail.com>
2020-01-24 14:54:21 -0800

    Try to add audio play on Inital submit wanted to commit so i have it saved in case i come back to it later

--- js/scripts.js
+++ js/scripts.js
@@ -7,37 +7,44 @@ const numFind = (input, name) => {
         let resultArr = []
         let temp = []
         for (let i = 1; i <= input; ++i) {
-            temp.push(i)
+            temp.push(i);
         }
         temp.forEach(number => {
             numArr = number.toString().split('')
-            if (numArr.includes('3')) resultArr.push(`I'm sorry, ${name}. I'm afraid I can't do that,`)
-            else if (numArr.includes('2')) resultArr.push('Beep!,')
-            else if (numArr.includes('1')) resultArr.push('Boop!,')
-            else resultArr.push(`${number},`)
+            if (numArr.includes('3')) resultArr.push(`I'm sorry, ${name}. I'm afraid I can't do that,`);
+            else if (numArr.includes('2')) resultArr.push('Beep!,');
+            else if (numArr.includes('1')) resultArr.push('Boop!,');
+            else resultArr.push(`${number},`);
+
         })
-        result = resultArr.join(' ')
+        result = resultArr.join(' ');
     }
     else {
-        result = "Oops! What you entered is not a number"
+        result = "Oops! What you entered is not a number";
     }
-    return result
+    return result;
 }
 
 
 //Front end Logic
 $(document).ready(() => {
     $('#form1').submit(event => {
-        event.preventDefault()
-        var input = parseInt($("#input2").val())
-        var name = $("#input1").val()
-        var result = numFind(input, name)
-        $('#display-result').text(result)
-        $('#form-jumbo').addClass('noDisplay')
-        $('#result-jumbo').removeClass('noDisplay')
-    })
+        event.preventDefault();
+        var input = parseInt($("#input2").val());
+        var name = $("#input1").val();
+        var result = numFind(input, name);
+        var audio = $("#mysoundclip")[0]
 
-    $("#resetBtn").click(function () {
-        location.reload(true);
+        audio.play()
+        $('#display-result').text(result);
+        $('#form-jumbo').addClass('noDisplay');
+        $('#result-jumbo').removeClass('noDisplay');
+
+
     })
+})
+
+$("#resetBtn").click(function () {
+    location.reload(true);
+})
 })
